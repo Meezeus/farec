d4361142c9416889e23c5bfce43ffaeab3f430f1
Meezeus <kac3131@gmail.com>
2022-02-07 19:08:39 +0000

    Change file reading to work with JRT

--- src/main/java/dudzinski/kacper/farec/StartScreenController.java
+++ src/main/java/dudzinski/kacper/farec/StartScreenController.java
@@ -1,13 +1,11 @@
 package dudzinski.kacper.farec;
 
-import java.io.*;
-import java.net.URISyntaxException;
+import java.io.IOException;
 import java.net.URL;
-import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
@@ -21,48 +19,37 @@ public class StartScreenController implements Initializable {
 
     // Reads the changelog file and sets the label in the start_screen.
     public void initialize(URL location, ResourceBundle resources) {
-        String entry = "";
-        BufferedReader reader = null;
         try {
+            // Get and read the changelog file.
+            URL changelogURL = App.class.getResource("changelog.txt");
+            List<String> changelogLines = Files.readAllLines(Paths.get(changelogURL.toURI()));
+
+            // Get the latest entry in the changelog and set it to the label.
+            String changelogEntry = "";
             boolean readingLatestChanges = false;
-            File changelog = new File(App.class.getResource("changelog.txt").toURI());
-            reader = new BufferedReader(new FileReader(changelog));
-            String line = reader.readLine();
-            while (line != null) {
-                entry += line + "\n";
-                line = reader.readLine();
-                if (line != null && !readingLatestChanges && line.matches("##\\s.*")) {
+            for (String line : changelogLines) {
+                if (!readingLatestChanges && line.matches("##\\s.*")) {
                     readingLatestChanges = true;
                 }
-                else if (line != null && readingLatestChanges && line.matches("##\\s.*")) {
+                else if (readingLatestChanges && line.matches("##\\s.*")) {
                       break;
                 }
+                changelogEntry += line + "\n";
             }
-            changelogChanges.setText(entry);
-        } catch (URISyntaxException e) {
+            changelogChanges.setText(changelogEntry);
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                if (reader != null)  {
-                    reader.close();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
         }
     }
 
-    public void openCreateFAWindow(ActionEvent event) throws IOException{
+    public void openCreateFAWindow() throws IOException {
         fxmlLoader = new FXMLLoader(App.class.getResource("create_FA_screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         Stage stage = (Stage) changelogChanges.getScene().getWindow();
         stage.setScene(scene);
     }
 
-    public void openCreateREWindow(ActionEvent event) throws IOException{
+    public void openCreateREWindow() throws IOException{
         fxmlLoader = new FXMLLoader(App.class.getResource("create_RE_screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         Stage stage = (Stage) changelogChanges.getScene().getWindow();
