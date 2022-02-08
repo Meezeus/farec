319e8a4ac05c3d324f6c6da79aa91a7c58301d4d
Meezeus <kac3131@gmail.com>
2022-02-08 19:45:31 +0000

    Update help window to reflect new parsing

--- src/main/java/dudzinski/kacper/farec/HelpScreenController.java
+++ src/main/java/dudzinski/kacper/farec/HelpScreenController.java
@@ -19,11 +19,11 @@ public class HelpScreenController implements Initializable {
         operatorsLabel.setText(operatorLabelText);
 
         helpLabel.setText("A valid regex expression can only contain letters, number, brackets and regex operators. " +
-                "The regex expression is parsed from left to right. As a result, expressions such as a+b+c are parsed as " +
-                "a+(b+c). Valid expressions must have a root operator: an operator that is not contained within any brackets " +
-                "(excluding outer brackets surrounding the whole expression). Because of the way parsing works, the STAR " +
-                "operator cannot be adjacent to other operators without brackets. For example, a*+b will not be considered " +
-                "valid, but (a*)+b and b+a* are both valid.");
+                "Valid expressions must have a root operator: an operator that is not contained within any brackets " +
+                "(excluding outer brackets surrounding the whole expression). The regex expression is parsed from " +
+                "left to right. As a result, expressions such as a+b+c are parsed as a+(b+c). When looking for a " +
+                "root operator, the UNION and CONCATENATION operators are favoured over the STAR operator. As a " +
+                "result, the expression a*+b is considered the same as (a*)+b.");
     }
 
 }
