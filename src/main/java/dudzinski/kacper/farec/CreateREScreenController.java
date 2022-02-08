80c821b66bf05b91e8a751db8d762f99f4330263
Meezeus <kac3131@gmail.com>
2022-02-08 02:35:47 +0000

    Implement parsing in controller

--- src/main/java/dudzinski/kacper/farec/CreateREScreenController.java
+++ src/main/java/dudzinski/kacper/farec/CreateREScreenController.java
@@ -1,7 +1,27 @@
 package dudzinski.kacper.farec;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class CreateREScreenController {
 
-    
+    public Parser parser = new Parser();
+    public TextField reInputField;
+    public Button parseButton;
+    public Label infoLabel;
+
+    public void parseRE(){
+        String regexString = reInputField.getText().replaceAll("\\s+","").trim();
+        System.out.println(regexString);
+        try {
+            RegularExpression regex = parser.parse(regexString);
+            System.out.println(regex);
+            infoLabel.setText("Regular expression is valid!");
+        }
+        catch (IllegalArgumentException e){
+            infoLabel.setText("Regular expression is invalid!");
+        }
+    }
 
 }
