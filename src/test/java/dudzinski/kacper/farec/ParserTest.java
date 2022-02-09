9cb368ce7bff3690e4e28566dac788997c79c068
Meezeus <kac3131@gmail.com>
2022-02-09 12:56:38 +0000

    Remove newline

--- src/test/java/dudzinski/kacper/farec/ParserTest.java
+++ src/test/java/dudzinski/kacper/farec/ParserTest.java
@@ -3,7 +3,6 @@ package dudzinski.kacper.farec;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
